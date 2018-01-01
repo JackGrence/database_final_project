3fa9115f231eb479058120a87a269c3cb174ac4a
JackGrence <green1181772@gmail.com>
2018-01-01 21:16:20 +0800

    fix bug

--- database_final/student/index.php
+++ database_final/student/index.php
@@ -40,9 +40,6 @@ foreach($data as $row => $value)
     echo '<option value="'.$value[0].'">'.$value[1].'</option>';
 }
 ?>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
   </select>
 </div>
   <div class="form-group row">
